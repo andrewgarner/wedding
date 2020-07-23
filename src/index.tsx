9876b7edd70a83704ae1dbf67144aff94713e372
Andrew Garner <andrew@andrewgarner.com>
2020-07-23 12:34:42 +0100

    Configure AWS Amplify

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,12 @@
+import Amplify from 'aws-amplify';
+import AWSExports from 'aws-amplify';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+Amplify.configure(AWSExports);
 
 ReactDOM.render(
   <React.StrictMode>
