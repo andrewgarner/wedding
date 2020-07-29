e1ccd18ffb4f3154939564395f721214d783b29c
Andrew Garner <andrew@andrewgarner.com>
2020-07-29 15:45:30 +0100

    Configure Storybook Viewport addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-a11y/register',
     '@storybook/addon-knobs/register',
+    '@storybook/addon-viewport/register',
     '@storybook/preset-create-react-app',
     {
       name: '@storybook/addon-docs',
