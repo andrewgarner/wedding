fd7b7bad520b9b2edfbfaa9ea44f35f867ec3287
Andrew Garner <andrew@andrewgarner.com>
2020-07-29 16:06:28 +0100

    Configure Storybook Source addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-a11y/register',
     '@storybook/addon-knobs/register',
+    '@storybook/addon-storysource',
     '@storybook/addon-viewport/register',
     '@storybook/preset-create-react-app',
     {
