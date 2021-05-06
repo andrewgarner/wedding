254f16e07be7ee158fd8072d251ab8ed8082d132
Andrew Garner <andrew@andrewgarner.com>
2021-05-06 22:08:01 +0100

    Bootstrap Next.js app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   env: {
+    browser: true,
     es2021: true,
     node: true,
   },
@@ -8,12 +9,18 @@ module.exports = {
     "plugin:jest/recommended",
     "plugin:jest-formatting/recommended",
     "prettier",
+    "prettier/react",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
     ecmaVersion: 12,
     sourceType: "module",
   },
   plugins: ["@typescript-eslint"],
-  rules: {},
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
 };
--- README.md
+++ README.md
@@ -4,6 +4,16 @@ Andrew & Hannah are getting married at Nancarrow Farm.
 
 ## Getting Started
 
+First, run the development server:
+
 ```bash
-npm install
+npm run dev
+# or
+yarn dev
 ```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+
+## Learn More
+
+- [Next.js documentation](https://nextjs.org/docs)
